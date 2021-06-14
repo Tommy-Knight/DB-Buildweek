41aea682e0245fc451e6947cf60b20881e3d216d
baxacsharp <78537820+baxacsharp@users.noreply.github.com>
2021-06-14 21:05:21 +0200

    creating pdf api done

--- src/services/posts/posts.js
+++ src/services/posts/posts.js
@@ -1,48 +1,48 @@
 //posts routes
-import express from "express";
-import createError from "http-errors";
-import { posts } from "../../db/db.js";
+import express from "express"
+import createError from "http-errors"
+import { posts } from "../../db/db.js"
 // import ReviewModel from "./schema.js"
 // import ProductModel from "../products/schema.js"
 // import q2m from "query-to-mongo"
 
-const postsRouter = express.Router();
+const postsRouter = express.Router()
 postsRouter
   .route("/")
   .get(async (req, res, next) => {
     try {
-      const data = await posts.findAll();
-      res.send(data);
+      const data = await posts.findAll()
+      res.send(data)
     } catch (e) {
-      console.log(e);
+      console.log(e)
       next(
         createError(500, "Oops something went wrong, please try again later")
-      );
+      )
     }
   })
   .post(async (req, res, next) => {
     try {
-      const data = await posts.create(req.body);
-      res.send(data);
+      const data = await posts.create(req.body)
+      res.send(data)
     } catch (e) {
-      console.log(e);
+      console.log(e)
       next(
         createError(500, "Oops something went wrong, please try again later")
-      );
+      )
     }
-  });
+  })
 
 postsRouter
   .route("/:id")
   .get(async (req, res, next) => {
     try {
-      const data = await posts.findByPk(req.params.id);
-      res.send(data);
+      const data = await posts.findByPk(req.params.id)
+      res.send(data)
     } catch (e) {
-      console.log(e);
+      console.log(e)
       next(
         createError(500, "Oops something went wrong, please try again later")
-      );
+      )
     }
   })
   .put(async (req, res, next) => {
@@ -50,26 +50,26 @@ postsRouter
       const postss = await posts.update(req.body, {
         returning: true,
         where: { id: req.params.id },
-      });
-      res.send(postss);
+      })
+      res.send(postss)
     } catch (e) {
-      console.log(e);
+      console.log(e)
       next(
         createError(500, "Oops something went wrong, please try again later")
-      );
+      )
     }
   })
   .delete(async (req, res, next) => {
     try {
       const postss = await posts.destroy({
         where: { id: req.params.id },
-      });
-      res.send("Deleted successfully");
+      })
+      res.send("Deleted successfully")
     } catch (e) {
-      console.log(e);
+      console.log(e)
       next(
         createError(500, "Oops something went wrong, please try again later")
-      );
+      )
     }
-  });
-export default postsRouter;
+  })
+export default postsRouter
